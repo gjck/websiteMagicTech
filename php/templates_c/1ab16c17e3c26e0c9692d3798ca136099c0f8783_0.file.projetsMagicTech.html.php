94498e9243ddcad2e534217f1360b2adbb2c8778
Gauthier JCK <gauthier.jenczak@gmail.com>
2016-09-26 15:05:32 +0200

    page projets OK

--- php/templates_c/1ab16c17e3c26e0c9692d3798ca136099c0f8783_0.file.projetsMagicTech.html.php
+++ php/templates_c/1ab16c17e3c26e0c9692d3798ca136099c0f8783_0.file.projetsMagicTech.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2016-09-26 14:14:37
+/* Smarty version 3.1.30, created on 2016-09-26 15:04:47
   from "/Applications/MAMP/htdocs/MagicTech/html/projetsMagicTech.html" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_57e9112d500a30_90080526',
+  'unifunc' => 'content_57e91cefbe9086_80158008',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '1ab16c17e3c26e0c9692d3798ca136099c0f8783' => 
     array (
       0 => '/Applications/MAMP/htdocs/MagicTech/html/projetsMagicTech.html',
-      1 => 1474892075,
+      1 => 1474895083,
       2 => 'file',
     ),
   ),
@@ -22,7 +22,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:../html/footer.html' => 1,
   ),
 ),false)) {
-function content_57e9112d500a30_90080526 (Smarty_Internal_Template $_smarty_tpl) {
+function content_57e91cefbe9086_80158008 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <!DOCTYPE html>
 <html>
@@ -51,28 +51,54 @@ function content_57e9112d500a30_90080526 (Smarty_Internal_Template $_smarty_tpl)
 ?>
 
 
-    <section id="centrageSlider">
-      <div id="slider">
-        <a href="#" class="control_next">></a>
-        <a href="#" class="control_prev"><</a>
-          <ul>
-            <li>SLIDE 1</li>
-            <li style="background: #aaa;">SLIDE 2</li>
-            <li>SLIDE 3</li>
-            <li style="background: #aaa;">SLIDE 4</li>
-          </ul>
-      </div>
-    </section>
+    <div class="middleBody">
+      <section id="centrageSlider">
+        <div id="slider">
+          <a href="#" class="control_next">></a>
+          <a href="#" class="control_prev"><</a>
+            <ul>
+              <li><a href="#projet1"><img src="../ressources/img/balance.jpg" alt="Système de reconnaissance RFID de MagicTech" /></a></li>
+              <li><a href="#projet2"><img src="../ressources/img/piano.jpg" alt="Piano jouant seul de MagicTech" /></a></li>
+              <li><a href="#projet3"><img src="../ressources/img/miroir.jpg" alt="Miroir effrayant de MagicTech" /></a></li>
+            </ul>
+        </div>
+      </section>
 
-    <section class="projet">
-      <p> Projet 1 </p>
-    </section>
+      <section class="projet">
+        <article id="projet1">
+          <p> Projet 1 </p>
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+              non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+        </article>
+        <article id="projet2">
+          <p> Projet 2 </p>
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+              non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+        </article>
+        <article id="projet3">
+          <p> Projet 3 </p>
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+              non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+        </article>
+      </section>
+    </div>
 
-    <!--
     <?php $_smarty_tpl->_subTemplateRender("file:../html/footer.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 
-  -->
+
   </body>
 </html>
 <?php }
