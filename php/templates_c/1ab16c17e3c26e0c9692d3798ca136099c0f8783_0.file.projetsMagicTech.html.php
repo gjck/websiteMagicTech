fb8d7a5e494a924c74429bf6648f3dfbf93935d6
Gauthier JCK <gauthier.jenczak@gmail.com>
2016-09-26 14:15:25 +0200

    Version stable avec slider problème de meP

--- php/templates_c/1ab16c17e3c26e0c9692d3798ca136099c0f8783_0.file.projetsMagicTech.html.php
+++ php/templates_c/1ab16c17e3c26e0c9692d3798ca136099c0f8783_0.file.projetsMagicTech.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2016-09-22 12:14:52
+/* Smarty version 3.1.30, created on 2016-09-26 14:14:37
   from "/Applications/MAMP/htdocs/MagicTech/html/projetsMagicTech.html" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_57e3af1c5ef3a0_32305406',
+  'unifunc' => 'content_57e9112d500a30_90080526',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '1ab16c17e3c26e0c9692d3798ca136099c0f8783' => 
     array (
       0 => '/Applications/MAMP/htdocs/MagicTech/html/projetsMagicTech.html',
-      1 => 1474539135,
+      1 => 1474892075,
       2 => 'file',
     ),
   ),
@@ -22,24 +22,57 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:../html/footer.html' => 1,
   ),
 ),false)) {
-function content_57e3af1c5ef3a0_32305406 (Smarty_Internal_Template $_smarty_tpl) {
+function content_57e9112d500a30_90080526 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <!DOCTYPE html>
 <html>
   <head>
     <title> Magic Tech : Nos projets </title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="../css/style.css" />
+    <link href="../css/global.css" rel="stylesheet" type="text/css" media="all">
+    <link href="../css/projetsMagicTech.css" rel="stylesheet" type="text/css" media="all">
   </head>
 
   <body>
+      <?php echo '<script'; ?>
+ type="text/javascript" src="../libraries/jquery.js"><?php echo '</script'; ?>
+>
+      <?php echo '<script'; ?>
+ type="text/javascript" src="../js/projetsMagicTech.js"><?php echo '</script'; ?>
+>
+
+    <div class="backgroundBorne">
+      <video autoplay id="video-fond">
+        <source type="video/mp4" src="../ressources/video-bg4.mp4" media="(orientation: landscape)">
+      </video>
+    </div>
+
     <?php $_smarty_tpl->_subTemplateRender("file:../html/header.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 
 
+    <section id="centrageSlider">
+      <div id="slider">
+        <a href="#" class="control_next">></a>
+        <a href="#" class="control_prev"><</a>
+          <ul>
+            <li>SLIDE 1</li>
+            <li style="background: #aaa;">SLIDE 2</li>
+            <li>SLIDE 3</li>
+            <li style="background: #aaa;">SLIDE 4</li>
+          </ul>
+      </div>
+    </section>
+
+    <section class="projet">
+      <p> Projet 1 </p>
+    </section>
+
+    <!--
     <?php $_smarty_tpl->_subTemplateRender("file:../html/footer.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 
+  -->
   </body>
 </html>
 <?php }
--- php/templates_c/7bf8606de2ad891bc94078f3a7e2a97b6143e9f2_0.file.connaitreMagicTech.html.php
+++ php/templates_c/7bf8606de2ad891bc94078f3a7e2a97b6143e9f2_0.file.connaitreMagicTech.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2016-09-26 10:39:35
+/* Smarty version 3.1.30, created on 2016-09-26 11:48:52
   from "/Applications/MAMP/htdocs/MagicTech/html/connaitreMagicTech.html" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_57e8dec7e3e5a6_34753830',
+  'unifunc' => 'content_57e8ef045e70a7_51946862',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '7bf8606de2ad891bc94078f3a7e2a97b6143e9f2' => 
     array (
       0 => '/Applications/MAMP/htdocs/MagicTech/html/connaitreMagicTech.html',
-      1 => 1474879168,
+      1 => 1474883321,
       2 => 'file',
     ),
   ),
@@ -22,7 +22,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:../html/footer.html' => 1,
   ),
 ),false)) {
-function content_57e8dec7e3e5a6_34753830 (Smarty_Internal_Template $_smarty_tpl) {
+function content_57e8ef045e70a7_51946862 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <!DOCTYPE html>
 <html>
