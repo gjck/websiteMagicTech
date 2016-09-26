79d9356fbc952789ab78e094ad8bf7af19fcec32
Gauthier JCK <gauthier.jenczak@gmail.com>
2016-09-26 09:36:01 +0200

    Session 26 09

--- php/templates_c/7bf8606de2ad891bc94078f3a7e2a97b6143e9f2_0.file.connaitreMagicTech.html.php
+++ php/templates_c/7bf8606de2ad891bc94078f3a7e2a97b6143e9f2_0.file.connaitreMagicTech.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2016-09-23 10:38:56
+/* Smarty version 3.1.30, created on 2016-09-23 14:45:14
   from "/Applications/MAMP/htdocs/MagicTech/html/connaitreMagicTech.html" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_57e4ea20753561_39937572',
+  'unifunc' => 'content_57e523da1cb4e9_42497978',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '7bf8606de2ad891bc94078f3a7e2a97b6143e9f2' => 
     array (
       0 => '/Applications/MAMP/htdocs/MagicTech/html/connaitreMagicTech.html',
-      1 => 1474619934,
+      1 => 1474622869,
       2 => 'file',
     ),
   ),
@@ -22,7 +22,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:../html/footer.html' => 1,
   ),
 ),false)) {
-function content_57e4ea20753561_39937572 (Smarty_Internal_Template $_smarty_tpl) {
+function content_57e523da1cb4e9_42497978 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <!DOCTYPE html>
 <html>
@@ -33,22 +33,140 @@ function content_57e4ea20753561_39937572 (Smarty_Internal_Template $_smarty_tpl)
   </head>
 
   <body>
-    
+
     <?php $_smarty_tpl->_subTemplateRender("file:../html/header.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 
 
-
-    <!--
     <section class="centrePage">
       <p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
         incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
         exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
         irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-         deserunt mollit anim id est laborum." </p>
+         deserunt mollit anim id est laborum.""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+           incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+           exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+           irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+            deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+              irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+               pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+               deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                 irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                  deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                     pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                     deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                       incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                       exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                       irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                        deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                           deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                             incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                             exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                             irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                              deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                 pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                 deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                   incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                   exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                   irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                    deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                      exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                      irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                       deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                         incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                         exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                         irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                          deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                             deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                               incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                               exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                               irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                                deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                                  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                                  irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                                   pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                                   deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                                     incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                                     exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                                     irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                                      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                                      deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                                        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                                         pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                                         deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                                           incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                                           exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                                           irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                                            deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                                              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                                              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                                              irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                                               pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                                               deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                                                 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                                                 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                                                 irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                                                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                                                  deserunt mollit anim id est laborum."Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                                                  deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                                                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                                                    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                                                     pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                                                     deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                                                       incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                                                       exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                                                       irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                                                        deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                                                          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                                                          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                                                          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                                                           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                                                           deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                                                             incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                                                             exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                                                             irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                                                              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                                                              deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                                                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                                                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                                                                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                                                                 pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                                                                 deserunt mollit anim id est laborum."   </p>
     </section>
-  -->
 
   <?php $_smarty_tpl->_subTemplateRender("file:../html/footer.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
